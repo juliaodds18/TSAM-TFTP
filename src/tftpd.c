4e94ba470ca47daa55374a975796f7979c6337cb
sandraos15 <sandraos15@skel.ru.is>
2017-09-05 14:28:32 +0000

    Made the rand function in tftpd.c

--- src/tftpd.c
+++ src/tftpd.c
@@ -1,6 +1,25 @@
 /* your code goes here. */
 
+/*
+ * Returns a random PORT number between 49152 and 65535
+ */
+
+/* MIN and MAX portnunbers */
+int MINPORT = 49152;
+int MAXPORT = 65535;
+
+int randomChosenTID(){
+        return (rand()%(MAXPORT-MINPORT))+MINPORT;
+}
+
 int main(int argc, char *argv[])
 {
+	int port = randomChosenTID();
+        int sockfd;
+        //print(port);
 	return 0;
 }
+
+// The output of the server should list which file is requested from which IP and portb
+// // the server should only send files that are inside the directory given as command line argument
+// // The RRQ and WRQ have a mode string
