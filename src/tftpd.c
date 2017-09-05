1ecc15dd68fb17ef6d6f9b888db70ce3f4016b15
juliao15 <juliao15@skel.ru.is>
2017-09-05 15:49:22 +0000

    Added more code

--- src/tftpd.c
+++ src/tftpd.c
@@ -20,33 +20,50 @@ int MAXPORT = 65535;
 // Structs for client and server
 struct sockaddr_in server, client;
 
+// Character array for message; 
+char message[512]; 
+
 /* * * * * * * * * * * *
         Functions
  * * * * * * * * * * * * */
 
-// Returns a random PORT number between 49153 and 65535
-int randomChosenTID(){
-    return (rand()%(MAXPORT-MINPORT))+MINPORT;
-}
 
 int main(int argc, char *argv[])
 {
+    // If parameters are strictly less than one, there are no userful parameters 
+    // and nothing to be done.
     if (argc < 1) {
         return 0; 
     } 
- 
-    int port = randomChosenTID();
+
+    int port;  
+    sscanf(argv[1], "%d", &port);
     int sockfd;
     // printf("The port number is: %d", port);
 	
     // Create and bind an UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
     memset(&server, 0, sizeof(server)); 
+    // Address family = IPv4, type of addresses that socket can communicate with
     server.sin_family = AF_INET; 
     
     // Convert host byte order to network byte order
     server.sin_addr.s_addr = htonl(INADDR_ANY); 
-    server.sin_port = htons(randomChosenTID()); 
+    server.sin_port = htons(port);
+    bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));  
+    
+    for (;;) {
+
+        // Receive up to one byte less than declared, will be NULL-terminated later
+        socklen_t len = (socklen_t) sizeof(client); 
+        ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1, 
+                             0, (struct sockaddr *) &client, &len); 
+        message[n] = '\0'; 
+        
+        
+    }
+
+
     return 0;
 }
 
